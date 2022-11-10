07f8f9bc78fae6946ea0936e962e688983561cc1
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2022-11-10 12:59:09 +0100

    Added some more functionality

--- script.js
+++ script.js
@@ -8,17 +8,26 @@ addTodoBtn.addEventListener("click", () => {
         newTodo.classList.add("Todo-item");
 
         newTodo.innerHTML = `
-            <div class="Todo-text">
-                <p> ${inputBox.value} </p>
+            <div class="main">
+                <div class="Todo-text">
+                    <p> ${inputBox.value} </p>
+                </div>
+                
+                <div class="actionBtns">
+                    <i title="Edit to-do item" class="fa-solid fa-pen-to-square"></i>
+                    <i class="fa-solid fa-rotate-left"></i>
+                    <i title="Finish to-do item" class="fa-solid fa-check"></i>
+                    <i title="Delete to-do item" class="fa-solid fa-trash"></i>
+                </div>
             </div>
-            
-            <div class="actionBtns">
-                <i title="Edit to-do item" class="fa-solid fa-pen-to-square"></i>
-                <i title="Finish to-do item" class="fa-solid fa-square-check"></i>
-                <i title="Delete to-do item" class="fa-solid fa-trash"></i>
+
+            <div class="updateToggler">
+                <div class="update">
+                    <input type="text" id="inputBoxEdit" placeholder="Enter your edited to-do item..." autocomplete="off">
+                    <button id="addTodoBtnEdit">Edit</button>
+                </div>
             </div>
         `
-
         todoItems.appendChild(newTodo);
         inputBox.value = "";
     }
@@ -36,5 +45,21 @@ todoItems.addEventListener("click", (e) => {
 })
 
 todoItems.addEventListener("click", (e) => {
-    
+    if (e.target.classList.contains("fa-check")) {
+        e.target.parentElement.parentElement.classList.toggle("completed");
+        e.target.parentElement.classList.toggle("done");
+    }
+})
+
+todoItems.addEventListener("click", (e) => {
+    if (e.target.classList.contains("fa-rotate-left")) {
+        e.target.parentElement.parentElement.classList.toggle("completed");
+        e.target.parentElement.classList.toggle("done");
+    }
+})
+
+todoItems.addEventListener("click", (e) => {
+    if (e.target.classList.contains("fa-pen-to-square")) {
+        e.target.parentElement.parentElement.parentElement.classList.toggle("Update")
+    }
 })
